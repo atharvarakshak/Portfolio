35ff300179d411742945072a9d20016d9fe3a89b
atharvarakshak <atharvarakshak23@gmail.com>
2024-06-07 21:21:12 +0530

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,10 +24,10 @@ export function Hero() {
       <div className='wrapper h-screen w-auto absolute top-0 left-0 right-0 opacity-100 z-0 p-4'>
       </div>
       <div className="hero-text max-h-screen w-full md:w-[50%] p-8 relative z-10 mt-8 text-left">
-        <ScrollAnimation animateIn="animate__fadeInUp" delay={200}>
+        <ScrollAnimation animateIn="animate__fadeInUp" delay={200} animateOnce={false}>
           <p className="flex text-[1.2rem] md:text-[1.8rem] w-full"> Hello  &nbsp;&nbsp;<img src={Hello} alt="Hello" className='w-8' />, I'm</p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="animate__fadeInUp" delay={600}>
+        <ScrollAnimation animateIn="animate__fadeInUp" delay={600} animateOnce={false}>
           <h1 className="text-4xl md:text-[3.5rem] text-white dark:text-gray-800 font-customFont mt-4">Atharva Rakshak</h1>
         </ScrollAnimation>
         <div className="text-xl md:text-3xl lg:text-4xl mt-4">
@@ -37,7 +37,7 @@ export function Hero() {
         <button className='rounded-2xl border-2 border-white p-2 mt-4 dark:border-gray-800 text-white bg-green-500'>
           Contact
         </button>
-        <ScrollAnimation animateIn='animate__flipInX' >
+        <ScrollAnimation animateIn="animate__fadeInUp" delay={1000} animateOnce={false}>
           <div className="social-media items-center flex justify-start gap-4 mt-4">
             <a href="https://www.linkedin.com/in/atharva-rakshak-b53025257/" target="_blank" rel="noreferrer">
               <img src={linkedin} alt="Linkedin" className="w-12" />
@@ -55,7 +55,11 @@ export function Hero() {
         </ScrollAnimation>
       </div>
       <div className="hero-image relative z-10 h-screen w-[50%] ml-4 hidden md:inline-block">
+      <ScrollAnimation animateIn="animate__fadeInRight" delay={500} >
+      <div className="hero-image relative z-10 h-screen  ml-4 hidden md:inline-block">
         <canvas id='canvas3d' className='h-full w-full'></canvas>
+      </div>
+      </ScrollAnimation>
       </div>
     </div>
   );
